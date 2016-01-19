5c5bbfeea5959bc204f0fcb15cebfc54eea42f7d
Paul Bradshaw <paulonhismobile@gmail.com>
2016-01-19 21:10:54 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -33,7 +33,7 @@ for td in tds:
 
 for td in tds:
      record = { "td" : td.text } # column name and value
-  #   scraperwiki.sqlite.save(["td"], record) # save the records one by one
+     scraperwiki.sqlite.save(["td"], record) # save the records one by one
     
 # -----------------------------------------------------------------------------
 # Go back to the Tutorials page and continue to Tutorial 3 to learn about 
