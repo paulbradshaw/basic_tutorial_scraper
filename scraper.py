5ee40fd03a80794380861009b61162538f6ebaa3
Paul Bradshaw <paulonhismobile@gmail.com>
2017-02-25 09:36:32 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,6 +20,7 @@ print "Click on the ...more link to see the whole page"
 import lxml.html
 root = lxml.html.fromstring(html) # turn our HTML into an lxml object
 tds = root.cssselect('td') # get all the <td> tags
+print tds #show the tds as lxml objects - below we'll convert them into text
 for td in tds:
     print lxml.html.tostring(td) # the full HTML tag
     print td.text                # just the text inside the HTML tag
